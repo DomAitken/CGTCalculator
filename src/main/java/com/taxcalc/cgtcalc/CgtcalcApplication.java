d6ddb0a3504d4beb0e1ddd374432fb24230d36db
Dom Aitken <daitken123@hotmail.com>
2025-03-25 19:52:47 +0000

    Commit 001: Project created and initial commit made

---
+++ src/main/java/com/taxcalc/cgtcalc/CgtcalcApplication.java
@@ -0,0 +1,13 @@
+package com.taxcalc.cgtcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgtcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgtcalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taxcalc/cgtcalc/CgtcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxcalc.cgtcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgtcalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
